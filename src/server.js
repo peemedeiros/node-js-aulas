1be46c429e8ba17b1a55b4db04b075440ddf1fcc
Pedro Medeiros <pedrohenriquelm500@gmail.com>
2020-02-07 15:23:55 -0300

    Server.js permitindo CORS

--- src/server.js
+++ src/server.js
@@ -3,10 +3,16 @@ const app = express()
 
 app.use(express.json())
 
+let allowCrossDomain = (req,res,next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+}
 
 const auth = require('./routes/authRoutes')
 const series = require('./routes/seriesRoutes')
 
+app.use(allowCrossDomain)
 app.use('/auth', auth)
 app.use('/series', series)
 
